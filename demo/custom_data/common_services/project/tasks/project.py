67b2e6ce7bb068cad32c57ce61c6d833a455a07a
Grayson Miller <grayson.miller124@gmail.com>
2024-05-12 21:18:02 -0600

    more project bs

--- demo/custom_data/common_services/project/tasks/project.py
+++ demo/custom_data/common_services/project/tasks/project.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import os
 from pathlib import Path
+from uuid import uuid4
 
 from dizzy import Task
 from dizzy.daemon.settings import data_root
@@ -10,10 +11,12 @@ PROJECT_ROOT = data_root.parent / "projects"
 
 @dataclass
 class Project:
-    id: str
-    name: str
-    description: str
-    root: str
+    id: str = field(default=uuid4().hex)
+    name: str = "New Project"
+    description: str = "A new project"
+    root: str = ""
+    paths: list[dict] = field(default_factory=list)
+    files: list[dict] = field(default_factory=list)
 
     def __post_init__(self):
         self.root = PROJECT_ROOT / self.id
@@ -30,7 +33,11 @@ class Project:
 class ProjectTask(Task):
     """Base class for tasks that interact with data"""
 
-    project: Project
+    # TODO: add extra_contexts to Task specifically
+    # extra_context = ["project"]
+
+    # would need to copy or unify ProjectTasks... :x
+    # project: Project = field(default_factory=Project)
 
     def add_to_context(self, ctx):
         ctx["project"] = self.project.to_dict()
@@ -42,58 +49,55 @@ class ProjectTask(Task):
         return cls
 
 
-class CreateProject(Task):
+class CreateProject(ProjectTask):
     """Create a project directory with a given UUID"""
 
     @staticmethod
     def run(ctx):
-        path: Path = PROJECT_ROOT / ctx["project"]["id"]
-        path.mkdir(parents=True, exist_ok=True)
-        return {"status": "success" if path.exists() else "failed", "path": path}
+        ctx["project"] = Project()
+        path = PROJECT_ROOT / ctx["project"].id
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+        return path
 
 
-class SetProjectDir(Task):
-    """Get the project directory of a given UUID"""
-
-    requested_actions = ["set_project_dir"]
+class ListProjects(ProjectTask):
+    """List all projects in the project directory"""
 
     @staticmethod
     def run(ctx):
-        ctx["project"]["root"] = PROJECT_ROOT / ctx["project"]["id"]
-        return ctx["project"]["root"]
+        project_ids = [p.name for p in PROJECT_ROOT.iterdir() if p.is_dir()]
+        ctx["project_ids"] = project_ids
+        return project_ids
 
 
-class ReadProjectFiles(Task):
+class ReadProjectFiles(ProjectTask):
     """Read a file from the project of a given UUID"""
 
+    dependencies = ["CreateProject"]
+
     @staticmethod
     def run(ctx):
-        path: Path = PROJECT_ROOT / ctx["project"]["id"] / ctx["filepath"]
+        files = []
+        for file in ctx["project"].paths:
+            path: Path = PROJECT_ROOT / ctx["project"].id / file["path"]
+            with open(path, "r") as f:
+                content = f.read()
+            files.append({"path": file["path"], "content": content})
+        ctx["project"].files = files
+        return files
 
-        if not os.path.exists(path):
-            raise FileNotFoundError(f"File {path} not found")
 
-        with open(path, "r") as f:
-            ctx["content"] = f.read()
-
-        return ctx["content"]
-
-
-class WriteProjectFiles(Task):
+class WriteProjectFiles(ProjectTask):
     """Write a file to the project of a given UUID"""
 
+    dependencies = ["CreateProject"]
+
     @staticmethod
     def run(ctx):
-        path: Path = data_root / "projects" / ctx["project"]["id"] / ctx["filepath"]
-
-        if not path.is_file():
-            raise TypeError(f"Path {path} is not a file")
-
-        if not os.path.exists(path):
+        for file in ctx["project"].files:
+            path: Path = PROJECT_ROOT / ctx["project"].id / file["path"]
             path.parent.mkdir(parents=True, exist_ok=True)
-            path.touch()
 
-        with open(path, "w") as f:
-            f.write(ctx["content"])
-
-        return ctx["content"]
+            with open(path, "w") as f:
+                f.write(file["content"])
--- demo/custom_data/entities/master/services/alef/tasks/data.py
+++ demo/custom_data/entities/master/services/alef/tasks/data.py
@@ -6,6 +6,9 @@ class IngestData(Task):
 
     @staticmethod
     def run(ctx):
+        return {"data": "Hello, world!"}
+        # needt o make some sort of action to grab data from somewhere
+        # or options to pass in data
         with open(ctx["file_path"], "r") as f:
             data = f.read()
         return {"data": data}
@@ -17,3 +20,11 @@ class ProcessData(Task):
     @staticmethod
     def run(ctx):
         return {"processed_data": ctx["data"].upper()}
+
+
+class AnalyzeResults(Task):
+    """Analyze processed data"""
+
+    @staticmethod
+    def run(ctx):
+        return {"analysis": "Data is all uppercase."}
